b3ec6c3bb9e91391dd05614d8c4f5737402f9ad1
Max McHarris <am7155@nyu.edu>
2019-04-05 15:50:31 -0400

    first commit!

---
+++ Assets/Scripts/PlantModel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantModel
+{
+    int WateredLevel;
+
+    MinMax Healthy;
+    MinMax Dead;
+
+    public PlantModel(int WateredLevel, MinMax Healthy, MinMax Dead)
+    {
+        this.WateredLevel = WateredLevel;
+        this.Healthy = Healthy;
+        this.Dead = Dead;
+    }
+
+    // Getters
+
+    public PlantStates GetState()
+    {
+        PlantStates state;
+        if (IsUnderwatered())
+        {
+            state = PlantStates.Underwatered;
+        }
+        else if (IsHealthy())
+        {
+            state = PlantStates.Healthy;
+        }
+        else if (IsOverWatered())
+        {
+            state = PlantStates.Overwatered;
+        }
+        else
+        {
+            state = PlantStates.Dead;
+        }
+        return state;
+    }
+
+    public bool IsUnderwatered()
+    {
+        return (Dead.min < WateredLevel && WateredLevel < Healthy.min);
+    }
+
+    public bool IsHealthy()
+    {
+        return (Healthy.min < WateredLevel && WateredLevel < Healthy.max);
+    }
+
+    public bool IsOverWatered()
+    {
+        return (Healthy.max < WateredLevel && WateredLevel < Dead.max);
+    }
+
+    public bool IsDead()
+    {
+        return (WateredLevel <= Dead.min || Dead.max <= WateredLevel);
+    }
+}
---
+++ Assets/Scripts/PlantTraits.cs
@@ -0,0 +1,22 @@
+﻿// SUMMARY - this file stores enums, structs associated with plants
+
+// states for plants
+public enum PlantStates
+{
+    Dead,
+    Underwatered,
+    Healthy,
+    Overwatered,
+}
+
+// struct which contains a Min, Max
+public struct MinMax
+{
+    public int min, max;
+
+    public MinMax(int min, int max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}
